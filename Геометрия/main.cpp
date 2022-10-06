9508ecd1072fd8f6f42c2bb7a5abfa1d63ec2b8a
marin <marin@LAPTOP-J2TPL7JK>
2022-10-06 21:22:57 +0300

    Geometry encoding

--- Геометрия/main.cpp
+++ Геометрия/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+ï»¿#include<iostream>
 using namespace std;
 //#define SQUARE
 //#define TRIANGLE_1
@@ -12,7 +12,7 @@ void main()
 	setlocale(LC_ALL, "");
 
 	int n;
-	cout << "Ââåäèòå êîëè÷åñòâî çâåçäî÷åê"; cin >> n;
+	cout << "Ð’Ð²ÐµÐ´Ð¸Ñ‚Ðµ ÐºÐ¾Ð»Ð¸Ñ‡ÐµÑÑ‚Ð²Ð¾ Ð·Ð²ÐµÐ·Ð´Ð¾Ñ‡ÐµÐº"; cin >> n;
 #ifdef SQUARE
 	for (int i = 0;i < n;i++)
 	{
